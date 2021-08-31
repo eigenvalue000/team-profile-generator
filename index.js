d8334ee67622718d488ec9dbd2eb87b50f7e9a0e
Garrett Kegel <ghkegel@gmail.com>
2021-08-31 14:42:25 -0700

    Fixed the require statements in the four class files so that they use the correct path. Also implemented an inquirer prompt template to be changed later.

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ const Intern = require('./lib/Intern');
 const inq = require('inquirer');
 const fs = require('fs');
 
-
+inq.prompt([
+    {
+        type: 'input',
+        name: 'somename',
+        message: 'hey hey hey'
+    }
+]).then((ans) => {
+    console.log(ans)
+}).catch((err) => {
+    if (err.isTtyError) {
+        console.log('An error occured.');
+    } else {
+        console.log('A different kind of error occured.');
+    }
+});
