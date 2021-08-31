8c893bc4ce47722b4fa6fbad228fc0664d11c8c9
Garrett Kegel <ghkegel@gmail.com>
2021-08-31 11:35:44 -0700

    Added the file system package to the set of imported packages.

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inq = require('inquirer');
+const fs = require('fs');
+
 
