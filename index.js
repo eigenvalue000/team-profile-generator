67229fd92c95affaba9591ed7ad7b32ce8e26e5c
Garrett Kegel <ghkegel@gmail.com>
2021-09-02 12:32:00 -0700

    Trying out an async function to call the manager prompt.

--- index.js
+++ index.js
@@ -3,41 +3,31 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inq = require('inquirer');
+inq.registerPrompt('recursive', require('inquirer-recursive'));
 const fs = require('fs');
 
-inq.prompt([
-    {
-        type: 'input',
-        name: 'managerName',
-        message: 'Enter the team manager\'s name : '
-    },
-    {
-        type: 'input',
-        name: 'managerId',
-        message: 'Enter the team manager\'s ID number : '
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'Enter the team manager\'s email address : '
-    },
-    {
-        type: 'input',
-        name: 'managerOfficeNumber',
-        message: 'Enter the team manager\'s office number : '
-    },
-    {
-        type: 'list',
-        name: 'optionsList',
-        message: 'Pick an option to continue : ',
-        choices: ['Add an engineer.', 'Add an intern.', 'Finish building team.']
-    },
-]).then((ans) => {
-    console.log(ans)
-}).catch((err) => {
-    if (err.isTtyError) {
-        console.log('An error occured.');
-    } else {
-        console.log('A different kind of error occured.');
-    }
-});
+const team = [];
+
+function managerPrompt() {
+    return new Promise(resolve => {
+        inq.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Manager name : '
+            },
+        ]).then((ans) => {
+            const manager = new Manager(ans.name, 1, 'gm', 1);
+            team.push(manager);
+            resolve(team)
+        })
+    })
+}
+
+async function asyncCall() {
+    console.log('calling');
+    const result = await managerPrompt();
+    console.log(result);
+}
+
+asyncCall();
