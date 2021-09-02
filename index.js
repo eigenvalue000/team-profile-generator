8b0f5cc67fc00726bdab60f0c3d7201d0589cca2
Garrett Kegel <ghkegel@gmail.com>
2021-09-02 13:09:51 -0700

    Added limited functionality to the employee prompt. Intern is incomplete. Engineer is complete.

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ function managerPrompt() {
             {
                 type: 'input',
                 name: 'name',
-                message: 'Manager name : '
+                message: 'Manager\'s name : '
             },
             {
                 type: 'input',
                 name: 'id',
-                message: 'Manager ID : ',
+                message: 'Manager\'s ID : ',
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'Manager email : ',
+                message: 'Manager\'s email : ',
             },
             {
                 type: 'input',
                 name: 'officeNumber',
-                message: 'Manager office number : ',
+                message: 'Manager\'s office number : ',
             }
         ]).then((ans) => {
             const manager = new Manager(ans.name, ans.id, ans.email, ans.officeNumber);
@@ -43,22 +43,46 @@ function employeePrompt() {
     return new Promise(resolve => {
         inq.prompt([
             {
-                type: 'list',
-                name: 'employeeType',
-                message: 'Add an employee to the team : ',
-                choices: ['Engineer', 'Intern',
+                type: 'recursive',
+                message: 'Would you like to add employees to your team?',
+                name: 'employeeTeam',
+                prompts: [
                     {
-                        name: 'Finish building team.',
-                        value: false
+                        type: 'list',
+                        name: 'employeeType',
+                        message: 'Add an employee to the team : ',
+                        choices: ['Engineer', 'Intern'],
+                    },
+                    {
+                        name: 'name',
+                        message: 'Engineer\'s name : ',
+                        when: ({ employeeType }) => employeeType === 'Engineer'
+                    },
+                    {
+                        name: 'id',
+                        message: 'Engineer\'s ID : ',
+                        when: ( {employeeType} ) => employeeType === 'Engineer'
+                    },
+                    {
+                        name: 'email',
+                        message: 'Engineer\'s email : ',
+                        when: ( {employeeType} ) => employeeType === 'Engineer'
+                    },
+                    {
+                        name: 'github',
+                        message: 'Engineer\'s Github : ',
+                        when: ( {employeeType} ) => employeeType === 'Engineer'
+                    },
+                    {
+                        name: 'name',
+                        message: 'Intern\'s name : ',
+                        when: ({ employeeType }) => employeeType === 'Intern'
                     }
                 ]
+
             }
         ]).then(ans => {
-            if (ans.employeeType) {
-                console.log('Selected an employee')
-            } else {
-                console.log('Building team.')
-            }
+           console.log(ans.employeeTeam.length)
         })
     })
 }
@@ -69,7 +93,7 @@ async function init() {
     const employeeResult = await employeePrompt();
     console.log(managerResult);
     console.log(employeeResult)
-    
+
 }
 
 init();
